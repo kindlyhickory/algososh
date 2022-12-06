b59944f61c9014c65e29be792b89cd346eab9287
kindlyhickory@gmail.com <58371570+kindlyhickory@users.noreply.github.com>
2022-12-06 21:03:19 +0300

    added stack e2e test

--- src/constants/testing-constants.ts
+++ src/constants/testing-constants.ts
@@ -1,5 +1,10 @@
-export const CIRCLE_CLASS_SELECTOR = '[data-testid=circle]'
-export const CIRCLE_LETTER_CLASS_SELECTOR = '[data-testid=circle-letter]'
-export const CIRCLE_TAIL_CLASS_SELECTOR = '[data-testid=circle-tail]'
-export const CIRCLE_HEAD_CLASS_SELECTOR = '[data-testid=circle-head]'
-export const CIRCLE_INDEX_CLASS_SELECTOR = '[data-testid=circle-index]'
+export const CIRCLE_CLASS_SELECTOR = '[data-testid=circle]';
+export const CIRCLE_LETTER_CLASS_SELECTOR = '[data-testid=circle-letter]';
+export const CIRCLE_TAIL_CLASS_SELECTOR = '[data-testid=circle-tail]';
+export const CIRCLE_MAIN_CLASS_SELECTOR = '[data-testid=circle-main]';
+export const CIRCLE_HEAD_CLASS_SELECTOR = '[data-testid=circle-head]';
+export const CIRCLE_INDEX_CLASS_SELECTOR = '[data-testid=circle-index]';
+export const REMOVE_BUTTON_SELECTOR = '[data-testid=remove-button]';
+export const ADD_BUTTON_SELECTOR = '[data-testid=add-button]';
+export const RESET_BUTTON_SELECTOR = '[data-testid=reset-button]';
+
